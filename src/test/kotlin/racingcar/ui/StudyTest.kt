36349bd9fe089209e50dc25a2ffe07be6f2e9ad5
mintjordy <mintjordy@woowahan.com>
2021-05-05 20:56:29 +0900

    [KOTLIN-RACING] String 학습테스트 추가

--- src/test/kotlin/racingcar/ui/StudyTest.kt
+++ src/test/kotlin/racingcar/ui/StudyTest.kt
@@ -5,7 +5,8 @@ import org.amshove.kluent.`should throw`
 import org.amshove.kluent.invoking
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import java.lang.NumberFormatException
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 @DisplayName("String extension 함수 학습테스트")
 internal class StudyTest {
@@ -54,5 +55,123 @@ internal class StudyTest {
 
         // then
         actual `should be equal to` 'j'
+    }
+
+    @Test
+    fun `repeat - 인자로 받는 숫자 만큼 append한 문자열을 반환`() {
+        // given
+        val data = "hollys"
+
+        // when
+        val actual = data.repeat(5)
+
+        // then
+        actual `should be equal to` "hollyshollyshollyshollyshollys"
+    }
+
+    @Test
+    fun `removeRange - 입력받은 range 범위에 따른 문자를 제거한 나머지 문자열을 반환`() {
+        // given
+        val data = "coffee"
+
+        // when
+        val actual = data.removeRange(0, 3)
+
+        // then
+        actual `should be equal to` "fee"
+    }
+
+    @Test
+    fun `removeRange - Range 타입의 범위에 따른 문자열 제거후 나머지 문자 반환`() {
+        // given
+        val data = "coffee"
+        val closedRange = IntRange(0, 3)
+
+        // when
+        val actual = data.removeRange(closedRange)
+
+        // then
+        actual `should be equal to` "ee"
+    }
+
+    @Test
+    fun `subsequence - subString과 같은 기능, 반환타입은 CharSequence이다`() {
+        // given
+        val data = "coffee"
+
+        // when
+        val actual = data.subSequence(0, 3)
+
+        // then
+        actual `should be equal to` "cof"
+    }
+
+    @Test
+    fun `subString - Java의 subString과 같은 기능`() {
+        // given
+        val data = "coffee"
+
+        // when
+        val actual = data.substring(0, 3)
+
+        // then
+        actual `should be equal to` "cof"
+    }
+
+    @Test
+    fun `subString - 인자가 Range Type, last 인덱스도 포함된다`() {
+        // given
+        val data = "coffee"
+        val range = IntRange(0, 3)
+
+        // when
+        val actual = data.substring(range)
+
+        // then
+        actual `should be equal to` "coff"
+    }
+
+    @Test
+    fun `capitalize - 첫 글자가 소문자인 경우 첫글자를 대문자로 변경된 새 문자열 반환`() {
+        // given
+        val data = "coffee is hot!"
+
+        // when
+        val actual = data.capitalize()
+
+        // then
+        actual `should be equal to` "Coffee is hot!"
+    }
+
+    @ParameterizedTest
+    @CsvSource("안녕하세요, 안녕하세요", "1Hello world, 1Hello world", ".name, .name")
+    fun `capitalize - 첫 글자가 소문자가 아닌 경우 같은 문자 반환`(data: String, expected: String) {
+        // when
+        val actual = data.capitalize()
+
+        // then
+        actual `should be equal to` expected
+    }
+
+    @Test
+    fun `capitalize - 첫 글자가 대문자인 경우 첫글자를 소문자로 변경된 새 문자열 반환`() {
+        // given
+        val data = "Coffee is hot!"
+
+        // when
+        val actual = data.decapitalize()
+
+        // then
+        actual `should be equal to` "coffee is hot!"
+    }
+
+    @ParameterizedTest
+    @CsvSource("안녕하세요, 안녕하세요", "1Hello world, 1Hello world", ".name, .name")
+    fun `capitalize - 첫 글자가 대문자가 아닌 경우 같은 문자 반환`(data: String, expected: String) {
+        // when
+        val actual = data.capitalize()
+
+        // then
+        actual `should be equal to` expected
     }
 }
